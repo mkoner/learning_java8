e5fb500f73b49ba3e731c3e55c42c558f3d804c9
mkoner <amzid@hotmail.fr>
2023-05-03 13:07:03 +0000

    Example1

--- lambda-expressions/Example1.java
+++ lambda-expressions/Example1.java
@@ -6,7 +6,18 @@ public class Example1{
 
     public static void main(String[] args){
         ArithmeticOperation addition = (a, b) -> a + b;
+        ArithmeticOperation subtraction = (int a, int b) -> a - b;
+        ArithmeticOperation mutiplication = (a, b) -> {return a * b;};
+        ArithmeticOperation division = (a, b) -> {
+   
+                return  a/b;
+            
+            
+        };
         System.out.println(operate(12, 4, addition));
+        System.out.println(operate(12, 4, subtraction));
+        System.out.println(operate(12, 4, mutiplication));
+        System.out.println(operate(12, 3, division));
     }
 
     private static int operate(int a, int b, ArithmeticOperation op){
