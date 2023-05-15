38cb67cc128dde8cb2ca7495540b04e1a9a6fb3e
mkoner <amzid@hotmail.fr>
2023-05-15 17:14:39 +0000

    Readme file

---
+++ streams/Example1.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Integer;
+
+public class Example1{
+
+    List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+
+    public static void main(String[] args){
+        
+    }
+}
