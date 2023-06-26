6d5d8a886c0c440cfc26f5fef49a52e39f352a73
mkoner <amzid@hotmail.fr>
2023-06-26 16:04:35 +0000

    Implemented filter method

--- streams/Example1.java
+++ streams/Example1.java
@@ -1,12 +1,16 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.lang.Integer;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Example1{
 
-    List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+    
 
     public static void main(String[] args){
-        
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5, 18);
+        Stream<Integer> oddNumbers = numbers.stream().filter(n->n%2!=0);
+        oddNumbers.forEach(System.out::println);
     }
 }
